f926bc0d9a814315ce150997590ce149c01492eb
monkeycoder456 <167249735+monkeycoder456@users.noreply.github.com>
2024-04-16 17:01:19 -0400

    Update towers.py
    updated barebones

--- towers.py
+++ towers.py
@@ -1,13 +1,15 @@
 import grid
 import pallets
 
+#setting up the towers, these are constant and thus will be constantly referenced.
 towwy = grid.matrix(6,5,'.')
-
-#write to point takes coords as (Y,X), begining from the top left, and the starting index is (0,0)
-
 towwy.writeToPoint('C',(5,4))
 towwy.writeToPoint('B',(5,2))
-towwy.writeToPoint('A',(5,0))
+towwy.feedtoColumn(['.','.','1','2','3','A'],0)
+
+#write to point takes coords as (Y,X), begining from the top left, and the starting index is (0,0)
+#these are just for show there is no logic
+
 towwy.drawGrid(1, pallets.drawkeyHanoi, 'lavender')
 
 print(towwy)
